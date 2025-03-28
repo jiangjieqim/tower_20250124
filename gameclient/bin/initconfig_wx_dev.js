88cacdd17a34b4ba4c1a1a961bfe474e35d83a5e
jiangjieqim@hotmai.com <jiangjieqim@hotmai.com>
2025-03-28 12:42:23 +0800

    '.'

---
+++ gameclient/bin/initconfig_wx_dev.js
@@ -0,0 +1,13 @@
+//微信灰度服务器
+var initConfig = {
+	//asset:"/Project1/Client/tower_20250124/resource/",
+	asset:"https://cdnserver-game.wanhuir.com/cbsg/tower_20250328/rev_out/",
+	platform:10,
+	sdk_platform:0,
+	sy_url:"https://server-game.wanhuir.com/tower",
+	proto_log:1,
+	ver:"tower_gray",
+	appid:"wxbca72f7c8bf7c7a8",
+	bundlebin:"bundle"
+}
+window["initConfig"] = initConfig;
